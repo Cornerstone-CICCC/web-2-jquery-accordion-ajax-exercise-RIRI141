762ef3392f7a48c3716ffac121076478bd20f697
Ryoga <isimaruru.1712@gmail.com>
2024-11-28 00:09:27 -0800

    done

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-$(function() {
+$(function () {
   // your code here
-})
+  $(".accordion-header").on("click", function () {
+    $(".accordion-content").slideUp();
+
+    const content = $(this).next(".accordion-content");
+    if (content.is(":visible")) {
+      content.slideUp();
+    } else {
+      content.slideDown();
+    }
+    // $(".accordion-header").not(this).slideDown();
+  });
+
+  $(".todos button").on("click", function () {
+    $.ajax({
+      url: "https://jsonplaceholder.typicode.com/todos",
+      method: "GET",
+      success: function (data) {
+        const todoList = $(".todos ul");
+        todoList.empty();
+        data.forEach((todos) => {
+          todoList.append(`
+            <li>
+            ${todos.title} 
+            ${todos.completed}
+            </li>
+            `);
+        });
+      },
+    });
+  });
+});
